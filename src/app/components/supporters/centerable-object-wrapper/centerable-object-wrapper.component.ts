470ab56ccfb31d3d08373fb1d8c6eb68b53aa844
itshui3 <xuhuizhu369@gmail.com>
2023-04-15 12:59:10 -0700

    player & deadblock basic style

--- src/app/components/supporters/centerable-object-wrapper/centerable-object-wrapper.component.ts
+++ src/app/components/supporters/centerable-object-wrapper/centerable-object-wrapper.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PiecesMap } from '../../pieces/pieces-map';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-centerable-object-wrapper',
   templateUrl: './centerable-object-wrapper.component.html',
   styleUrls: ['./centerable-object-wrapper.component.css'],
 })
-export class CenterableObjectWrapperComponent implements OnInit {
-  public piecesMap = PiecesMap;
-  public piece: string | undefined;
-
-  constructor(private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
-      if (params['id'] in PiecesMap) {
-        this.piece = params['id'];
-      } else {
-        console.error(`Component not found, please check URI: `, params['id']);
-      }
-    });
-  }
+export class CenterableObjectWrapperComponent {
+  constructor() {}
 }
